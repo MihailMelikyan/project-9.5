298c90ae896157def6f60d65aca157fc0768df9e
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-09-18 21:58:24 +0300

    выполненое задание

--- main.py
+++ main.py
@@ -3,46 +3,45 @@ class StepValueError(ValueError):
 
 
 class Iterator:
-    start = int
-    stop = int
-    step = 1
-    pointer = start
     def __init__(self,start,stop,step = 1):
         self.start = start
         self.stop = stop
         self.step = step
         if step == 0:
             raise StepValueError('Шаг не может быть равен 0')
+        if(step > 0 and start >= stop) or (step < 0 and start <= stop):
+            raise StepValueError('неверные параметры для итерации')
     def __iter__(self):
         self.pointer = self.start
         return self
     def __next__(self):
-        self.pointer += self.step
-        if self.pointer < self.stop:
-            return self.pointer
+        if (self.step > 0 and self.pointer < self.stop) or (self.step < 0 and self.pointer > self.stop):
+            current = self.pointer
+            self.pointer += self.step
+            return current
         raise StopIteration
-# try:
-#     iter1 = Iterator(100, 200, 0)
-# for i in iter1:
-#     print(i, end=' ')
-# except StepValueError:
-# print('Шаг указан неверно')
+try:
+    iter1 = Iterator(100, 200, 0)
+except StepValueError:
+    print('Шаг указан неверно')
 
 iter2 = Iterator(-5, 1)
 iter3 = Iterator(6, 15, 2)
-iter4 = Iterator(5, 1, -1)
+# iter4 = Iterator(5, 1, -1)
 iter5 = Iterator(10, 1)
-
 
 for i in iter2:
     print(i, end=' ')
-    print()
+print()
+
 for i in iter3:
     print(i, end=' ')
-    print()
+print()
+
 for i in iter4:
     print(i, end=' ')
-    print()
+print()
+
 for i in iter5:
     print(i, end=' ')
-    print()
+print()
